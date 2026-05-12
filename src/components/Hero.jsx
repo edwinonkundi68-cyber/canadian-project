b06c2f049b4972590be3b362e2ad36cbc8c8d000
Edwin Onkundi <edwinonkundi68@gmail.com>
2026-05-12 12:50:10 +0300

    feat:TopBar component complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
     return (
-        <div className="hero-section">  
-            <h1>Welcome to the Canadian Peace Project</h1>
+        <div className="text-center py-20 bg-green-600 text-white">  
+            <h1 className="text-3xl font-bold text-green-500s">Welcome to the Canadian Peace Project</h1>
             <p>Promoting Peace and Understanding Across Canada</p>
             <button className="hero-button">Learn More</button> 
         </div>
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,8 +1,27 @@
+import { FaFacebookF, FaYoutube, FaLinkedinIn, FaInstagram } from "react-icons/fa"
+
 const TopBar = () => {
-    return (
-        <div className="top-bar">
-            <p>Welcome to the Canadian Peace Project!</p>
+  return (
+    <div className="w-full bg-green-600 text-white text-xs">
+      <div className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center">
+        
+        {/* Left - Address */}
+        <span className="text-bold ">42123 Millbourne, Edmonton, AB</span>
+
+        {/* Center - Email */}
+        <span className="text-bold ">info@canadianpeacemakers.org</span>
+
+        {/* Right - Social Icons */}
+        <div className="flex gap-3 items-center">
+          <FaFacebookF className="cursor-pointer hover:opacity-75" size={14} />
+          <FaYoutube className="cursor-pointer hover:opacity-75" size={14} />
+          <FaLinkedinIn className="cursor-pointer hover:opacity-75" size={14} />
+          <FaInstagram className="cursor-pointer hover:opacity-75" size={14} />
         </div>
-    )
-}   
-export default TopBar       
+
+      </div>
+    </div>
+  )
+}
+
+export default TopBar
